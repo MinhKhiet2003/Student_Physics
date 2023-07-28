bff3502089ef2ae954199fc35b784ce8d594511b
Nguyen Duc Hung <hunglaiyen2k3@gmail.com>
2023-07-28 12:35:40 +0700

    Duc Hung - done Login

--- app/src/main/java/com/example/baitaplonquanlysinhvienhnue/ApiSevice.java
+++ app/src/main/java/com/example/baitaplonquanlysinhvienhnue/ApiSevice.java
@@ -7,6 +7,7 @@ import retrofit2.http.Body;
 import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 public interface ApiSevice {
@@ -18,5 +19,12 @@ public interface ApiSevice {
     Call<List<User>> getUsers();
     @DELETE("students/{id}")
     Call<Void> deleteUser(@Path("id") String studentId);
+
+    @POST("register")
+    Call<Void> register(@Body User user);
+    @GET("login")
+    Call<List<User>> getUser();
+    @PUT("fogotpassword")
+    Call<Void> fogotpassword(@Body User user);
 
 }
